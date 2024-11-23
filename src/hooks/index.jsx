c6676db53d6da31a9918b7d092d47257abb0d567
Harry <harry811016@gmail.com>
2024-11-23 19:38:42 +0800

    feat: add integrate favicon

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -12,17 +12,35 @@ const useFavicon = () => {
       document.head.appendChild(newFavicon);
     }
 
+    const brandConfigs = {
+      lawin: {
+        favicon: `${process.env.PUBLIC_URL}/lawin/favicon.ico`,
+        title: "LAWINPLAY SLOT MACHINE",
+      },
+      lodibet: {
+        favicon: `${process.env.PUBLIC_URL}/lodibet/favicon.ico`,
+        title: "LODIBET SLOT MACHINE",
+      },
+      integrate: {
+        favicon: `${process.env.PUBLIC_URL}/integrate/favicon.ico`,
+        title: "INTEGRATED LOTTERY SYSTEM",
+      },
+    };
+
     const path = location.pathname;
+
     if (path.includes("/lawin")) {
-      document.querySelector(
-        "link[rel='icon']"
-      ).href = `${process.env.PUBLIC_URL}/lawin/favicon.ico`;
-      document.title = "LAWINPLAY SLOT MACHINE";
+      document.querySelector("link[rel='icon']").href =
+        brandConfigs.lawin.favicon;
+      document.title = brandConfigs.lawin.title;
     } else if (path.includes("/lodibet")) {
-      document.querySelector(
-        "link[rel='icon']"
-      ).href = `${process.env.PUBLIC_URL}/lodibet/favicon.ico`;
-      document.title = "LODIBET SLOT MACHINE";
+      document.querySelector("link[rel='icon']").href =
+        brandConfigs.lodibet.favicon;
+      document.title = brandConfigs.lodibet.title;
+    } else if (path.includes("/integrate")) {
+      document.querySelector("link[rel='icon']").href =
+        brandConfigs.integrate.favicon;
+      document.title = brandConfigs.integrate.title;
     }
   }, [location]);
 };
